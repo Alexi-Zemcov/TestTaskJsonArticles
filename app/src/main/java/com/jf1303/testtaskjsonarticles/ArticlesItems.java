e901f47a2614a43407b045fa6309a3678cf17a21
Alexey <alexi.zemcov@gmail.com>
2020-07-14 07:22:57 +0400

    Вывод статей на экран. Модель статьи
    Добавлен класс модели "ArticlesItems"

--- app/src/main/java/com/jf1303/testtaskjsonarticles/ArticlesItems.java
+++ app/src/main/java/com/jf1303/testtaskjsonarticles/ArticlesItems.java
@@ -1,5 +1,31 @@
 package com.jf1303.testtaskjsonarticles;
 
 public class ArticlesItems {
-    private String m
+    private String mTitle;
+    private String mBody;
+    private String mId;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public void setBody(String mBody) {
+        this.mBody = mBody;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
 }
