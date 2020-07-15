bfa71597b09f71aaee735f6faf2e6ad4acb5bf1b
Alexey <alexi.zemcov@gmail.com>
2020-07-15 15:03:53 +0400

    Начал "переходить" на фрагменты
    Начал "переходить" на фрагменты, а потом решил откатиться и начать с RecyclerView

---
+++ app/src/main/java/com/jf1303/testtaskjsonarticles/ArticlesActivity.java
@@ -0,0 +1,13 @@
+package com.jf1303.testtaskjsonarticles;
+
+import androidx.fragment.app.Fragment;
+
+public class ArticlesActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
+
+
---
+++ app/src/main/java/com/jf1303/testtaskjsonarticles/ArticlesFragment.java
@@ -0,0 +1,7 @@
+package com.jf1303.testtaskjsonarticles;
+
+import androidx.fragment.app.Fragment;
+
+public class ArticlesFragment extends Fragment {
+
+}
